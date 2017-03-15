05e2ee8399fe9a930209ab58ce86de6b88acc13b
Ethan Hasson <attila135786@gmail.com>
2017-03-14 22:46:33 -0500

    Moved everything into the player script for now. Also created something similar to a gun socket for our bullets to spawn out of. Still need to add movement to the shot bullets.

--- StuckFightingZombies/Assets/Scripts/Player.cs
+++ StuckFightingZombies/Assets/Scripts/Player.cs
@@ -2,16 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PointAtTouch : MonoBehaviour {
+public class Player : MonoBehaviour {
+
+	public GameObject bulletSocket;
 
 	// Use this for initialization
 	void Start () {
-		Debug.Log ("Started");
+		//GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
+		//cube.transform.position = bulletSocket.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
 		if (Input.GetMouseButtonDown(0)) {
 			//transform.Rotate (Vector3.forward * Time.deltaTime * 10);
 			Vector2 mousePosition = Input.mousePosition;
@@ -23,6 +25,12 @@ public class PointAtTouch : MonoBehaviour {
 
 			Debug.Log (angle);
 			transform.rotation = Quaternion.Euler (0, 0, angle);
+			GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
+			cube.transform.localScale -= new Vector3 (.96f, .96f, .96f);
+			cube.transform.position = bulletSocket.transform.position;
+			cube.transform.rotation = bulletSocket.transform.rotation;
+
+
 		}
 	}
 }
